bb1a201e880b4b1aea702f8e23b5eb297faa2ca9
u22500172 <u22500172@tuks.co.za>
2023-04-26 18:45:45 +0200

    No Major Changes

--- mbaliStudyGroup/Controllers/StudyGroupController.cs
+++ mbaliStudyGroup/Controllers/StudyGroupController.cs
@@ -14,25 +14,14 @@ namespace mbaliStudyGroup.Controllers
     public class StudyGroupController : Controller
     {
         // GET: StudyGroup
-        List<Models.StudyGroup> studyGroup = new List<Models.StudyGroup>();
+        List<StudyGroup> studyGroup = new List<StudyGroup>();
 
 
         public ActionResult StudyGroup(Models.StudyGroup study)
         {
+            studyGroup.Add(new StudyGroup() { StudentNumber = study.StudentNumber, FirstName = study.FirstName, LastName = study.LastName, EmailAddress = study.EmailAddress, Delete = "Delete" });
+            ViewData["group"] = studyGroup;
 
-
-            //studyGroup.Add(new Models.StudyGroup { StudentNumber = "u22500172", FirstName = "Mbali", LastName = "Nkosi", EmailAddress = "u22500172@tuks.co.za", Delete = "Delete" });
-            // studyGroup.Add(new Models.StudyGroup { StudentNumber = "u12345678", FirstName = "John", LastName = "Smith", EmailAddress = "u12345678@tuks.co.za", Delete = "Delete" });
-            // studyGroup.Add(new Models.StudyGroup { StudentNumber = "u22334455", FirstName = "Thando", LastName = "Dlamini", EmailAddress = "u22334455@tuks.co.za", Delete = "Delete" });
-            //studyGroup.Add(new Models.StudyGroup { StudentNumber = "u21456789", FirstName = "Angela", LastName = "Khoza", EmailAddress = "u21456789@tuks.co.za", Delete = "Delete" });
-            studyGroup.Add(new Models.StudyGroup { StudentNumber = "u19202122", FirstName = "Thabo", LastName = "Tau", EmailAddress = "u19202122@tuks.co.za", Delete = "Delete" });
-            // studyGroup.Add(new Models.StudyGroup { StudentNumber = studentNum, FirstName = name, LastName =  surname, EmailAddress= email, Delete = "Delete" });
-
-           
-
-                studyGroup.Add(new StudyGroup() { StudentNumber = study.StudentNumber, FirstName = study.FirstName, LastName = study.LastName, EmailAddress = study.EmailAddress, Delete = "Delete" }); 
-                ViewData["group"] = studyGroup;
-           
             return View();
         }
 
